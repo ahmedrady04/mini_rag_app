0382e45af4fc45de3c2b753055d8eb13bc40330f
ahmedrady04 <ahmedrady04@gmail.com>
2025-11-09 14:31:57 +0000

    update 7

--- src/models/enums/__init__.py
+++ src/models/enums/__init__.py
@@ -1,1 +1,2 @@
 from .ResponseEnums import ResponseSignal
+from .ProcessingEunm import ProcessingEnum
--- src/routes/data.py
+++ src/routes/data.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI ,APIRouter,Depends ,UploadFile ,status
 from fastapi.responses import JSONResponse 
 import os
 from helpers.config import get_settings, Settings
-from controllers import DataController
-from controllers import ProjectController
+from controllers import DataController, ProjectController, ProcessController
 from aiofile import async_open
 from models import ResponseSignal
 import logging
+from .schemes.data import ProcessRequest
+
 
 logger = logging.getLogger("uvicorn.error")
 
@@ -24,7 +25,7 @@ async def upload_data(project_id:str,file:UploadFile,
 
   #validate the file properties
   data_controller=DataController()
-  is_valid ,result_signal =data_controller.validatje_uploaded_file(file=file)
+  is_valid ,result_signal =data_controller.validate_uploaded_file(file=file)
 
   if not is_valid:
       return JSONResponse(
@@ -66,3 +67,32 @@ async def upload_data(project_id:str,file:UploadFile,
         "file_id":file_id
     }
    )
+
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+   file_id= process_request.file_id
+   chunk_size=process_request.chunk_size
+   overlap_size=process_request.overlap_size
+
+   process_controller=ProcessController(project_id=project_id) 
+
+   file_content=process_controller.get_file_content(file_id=file_id)
+   
+   file_chunks=process_controller.process_file_content(
+      file_content=file_content,
+      file_id=file_id,
+      chunk_size=chunk_size,
+      overlap_size=overlap_size
+   )
+   if file_content is None or len(file_chunks)==0:
+      return JSONResponse(
+         status_code=status.HTTP_400_BAD_REQUEST,
+         content={
+            "status":"failed",
+            "signal":ResponseSignal.FILE_PROCESSING_FAILED.value,
+            "error":"Unsupported file type"
+         }
+      )
+   return file_chunks
+
